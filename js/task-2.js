b94aff4b5046c8f98723cdf44933c0346655bb26
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-03-02 20:52:34 +0100

    goit-js-hw-06

--- js/task-2.js
+++ js/task-2.js
@@ -1,64 +1,23 @@
-const getUsersWithFriend = (users, friendName) => {
-    
-    return users.filter(user => user.friends.includes(friendName));
-   
- }
+class Storage {
+  #item
+  constructor (item) {
+    this.#item = item
+  }
+  getItems() {
+return this.#item;
+  }
+  addItem(newItem) {
+return this.#item.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    return this.#item = this.#item.filter(item => item !== itemToRemove);
+  }
+}
 
 
-const allUsers = [
-    {
-      name: "Moore Hensley",
-      friends: ["Sharron Pace"]
-    },
-    {
-      name: "Sharlene Bush",
-      friends: ["Briana Decker", "Sharron Pace"]
-    },
-    {
-      name: "Ross Vazquez",
-      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
-    },
-    {
-      name: "Elma Head",
-      friends: ["Goldie Gentry", "Aisha Tran"]
-    },
-    {
-      name: "Carey Barr",
-      friends: ["Jordan Sampson", "Eddie Strong"]
-    },
-    {
-      name: "Blackburn Dotson",
-      friends: ["Jacklyn Lucas", "Linda Chapman"]
-    },
-    {
-      name: "Sheree Anthony",
-      friends: ["Goldie Gentry", "Briana Decker"]
-    }
-  ];
-  
- 
-  console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
-  // [
-  //   {
-  //     name: "Sharlene Bush",
-  //     friends: ["Briana Decker", "Sharron Pace"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-  console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
-  // [
-  //   {
-  //     name: "Elma Head",
-  //     friends: ["Goldie Gentry", "Aisha Tran"]
-  //   },
-  //   {
-  //     name: "Sheree Anthony",
-  //     friends: ["Goldie Gentry", "Briana Decker"]
-  //   }
-  // ]
-  
-  console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
