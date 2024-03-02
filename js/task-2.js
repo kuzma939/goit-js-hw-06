1455aec418aa34988ed2536604c90683bb8ec8d6
kuzma939 <135020134+kuzma939@users.noreply.github.com>
2024-03-02 22:31:40 +0100

    goit-js-hw-06

--- js/task-2.js
+++ js/task-2.js
@@ -1,16 +1,16 @@
 class Storage {
-  #item
+  #item;
   constructor (item) {
-    this.#item = item
+    this.#item = item;
   }
   getItems() {
-return this.#item
+return this.#item;
   }
   addItem(newItem) {
-this.#item.push(newItem)
+this.#item.push(newItem);
   }
   removeItem(itemToRemove) {
- this.#item = this.#item.filter(item => item !== itemToRemove)
+ this.#item = this.#item.filter(item => item !== itemToRemove);
   }
 }
 
